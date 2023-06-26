713bc670a272a3af965b96321281e5cd80328b02
yoshimoto <motokiti2011@gmail.com>
2023-06-26 21:39:28 +0900

    626 work

--- lib/service/api_client.dart
+++ lib/service/api_client.dart
@@ -6,10 +6,15 @@ import 'package:http/http.dart' as http;
 
 class ApiClient {
   Future<List<Product>?> fetchProducts() async {
-    final url =
-        Uri.parse('https://hausn48fya.execute-api.us-east-1.amazonaws.com/dev');
+    final url = Uri.parse(
+        'https://hausn48fya.execute-api.us-east-1.amazonaws.com/dev/productlist');
+
+    var request = new SampleRequest(query: 'QUERY', category: 'food');
+
     try {
-      final response = await http.get(url);
+      final response = await http.post(url,
+          body: json.encode(request.toJson()),
+          headers: {"Content-Type": "application/json"});
       if (response.statusCode == 200) {
         final List<dynamic> body = jsonDecode(response.body);
         final List<Product> products =
@@ -24,4 +29,19 @@ class ApiClient {
     }
     return null;
   }
+}
+
+class SampleRequest {
+  final String query;
+  final String category;
+  SampleRequest({
+    required this.query,
+    required this.category,
+  });
+  Map<String, dynamic> toJson() => {
+        'OperationType': query,
+        'Keys': {
+          'productCategory': category,
+        }
+      };
 }
